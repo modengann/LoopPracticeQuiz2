06c04a5153ea1530a78c60899bd7ab7f91529e78
modengann <99417989+modengann@users.noreply.github.com>
2023-09-25 21:11:30 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/**
+ *  Write code that reads a series of test scores (as integers) from the user until the user 
+ *  enters -1. Keep track of the highest and lowest test score. When the user enters '-1', print out
+ *  the highest and lowest scores with an appropriate message i.e. "The high score was ...."
+ */
+
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
